97825f11c9fae1c2615c6cd24ff0f3bf19400fac
Ege Bircan <ebircan@kocfinans.com.tr>
2021-07-27 11:42:52 +0300

    file CRUD

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -1,31 +1,55 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
 import com.udacity.jwdnd.course1.cloudstorage.model.CredentialForm;
+import com.udacity.jwdnd.course1.cloudstorage.model.FileForm;
+import com.udacity.jwdnd.course1.cloudstorage.model.FileInfo;
 import com.udacity.jwdnd.course1.cloudstorage.model.NoteForm;
 import com.udacity.jwdnd.course1.cloudstorage.services.CredentialService;
 import com.udacity.jwdnd.course1.cloudstorage.services.NoteService;
+import com.udacity.jwdnd.course1.cloudstorage.services.StorageService;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/home")
 public class HomeController {
     NoteService noteService;
     CredentialService credentialService;
+    StorageService storageService;
+    Boolean uploadError = false;
 
-    public HomeController(NoteService noteService, CredentialService credentialService) {
+    public HomeController(NoteService noteService, CredentialService credentialService, StorageService storageService) {
         this.noteService = noteService;
         this.credentialService = credentialService;
+        this.storageService = storageService;
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        storageService.deleteAll();
+        storageService.init();
     }
 
     @GetMapping()
     public String getHomePage(Authentication authentication, Model model) {
+        model.addAttribute("files", this.getListFiles());
         model.addAttribute("notes", noteService.getNotes(authentication.getName()));
         model.addAttribute("credentials", credentialService.getCredentials(authentication.getName()));
+
+        if (this.uploadError) {
+            model.addAttribute("uploadError", true);
+            this.uploadError = false;
+        }
 
         return "home";
     }
@@ -66,6 +90,47 @@ public class HomeController {
     @PostMapping("/deleteCredential")
     public String deleteCredential(CredentialForm credentialForm, Model model) {
         credentialService.deleteCredential(credentialForm);
+        return "redirect:" + "/home";
+    }
+
+
+    @PostMapping("/upload")
+    public String uploadFile(@ModelAttribute FileForm fileForm, Model model) {
+        if (storageService.load(fileForm.getFile().getOriginalFilename()) != null) {
+            this.uploadError = true;
+            return "redirect:" + "/home";
+        }
+
+        try {
+            storageService.save(fileForm.getFile());
+        } catch (Exception ignored) { }
+
+        return "redirect:" + "/home";
+    }
+
+    @GetMapping("/files")
+    public List<FileInfo> getListFiles() {
+        List<FileInfo> fileInfos = storageService.loadAll().map(path -> {
+            String filename = path.getFileName().toString();
+            String url = MvcUriComponentsBuilder
+                    .fromMethodName(HomeController.class, "getFile", path.getFileName().toString()).build().toString();
+
+            return new FileInfo(filename, url);
+        }).collect(Collectors.toList());
+
+        return fileInfos;
+    }
+
+    @GetMapping("/files/{filename:.+}")
+    public ResponseEntity<Resource> getFile(@PathVariable String filename) {
+        Resource file = storageService.load(filename);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+    }
+
+    @PostMapping("/deleteFile")
+    public String deleteFile(FileInfo fileInfo, Model model) {
+        storageService.deleteFile(fileInfo);
         return "redirect:" + "/home";
     }
 }
---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/StorageService.java
@@ -0,0 +1,74 @@
+package com.udacity.jwdnd.course1.cloudstorage.services;
+
+import com.udacity.jwdnd.course1.cloudstorage.model.FileInfo;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.util.FileSystemUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+@Service
+public class StorageService {
+
+    private final Path root = Paths.get("uploads");
+
+    public void init() {
+        try {
+            Files.createDirectory(root);
+        } catch (IOException e) {
+            throw new RuntimeException("Could not initialize folder for upload!");
+        }
+    }
+
+    public void save(MultipartFile file) {
+        try {
+            Files.copy(file.getInputStream(), this.root.resolve(file.getOriginalFilename()));
+        } catch (Exception e) {
+            throw new RuntimeException("Could not store the file. Error: " + e.getMessage());
+        }
+    }
+
+    public Resource load(String filename) {
+        try {
+            Path file = root.resolve(filename);
+            Resource resource = new UrlResource(file.toUri());
+
+            if (resource.exists() || resource.isReadable()) {
+                return resource;
+            } else {
+                return null;
+            }
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Error: " + e.getMessage());
+        }
+    }
+
+    public void deleteAll() {
+        FileSystemUtils.deleteRecursively(root.toFile());
+    }
+
+    public void deleteFile(FileInfo fileInfo) {
+        Resource resource = this.load(fileInfo.getName());
+
+        try {
+            resource.getFile().delete();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    public Stream<Path> loadAll() {
+        try {
+            return Files.walk(this.root, 1).filter(path -> !path.equals(this.root)).map(this.root::relativize);
+        } catch (IOException e) {
+            throw new RuntimeException("Could not load the files!");
+        }
+    }
+}
